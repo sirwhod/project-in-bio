973e4629ae36047fa9c17b705248f8174940495a
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-01-08 22:17:10 -0300

    class: Google config e implementando autenticação

---
+++ app/actions/manage-auth.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import { auth, signIn, signOut } from "../lib/auth"
+
+export async function manageAuth() {
+  const session = await auth()
+
+  if (!session) {
+    return await signIn("google", { redirectTo: "/create" })
+  }
+
+  return await signOut({ redirectTo: "/" })
+}
