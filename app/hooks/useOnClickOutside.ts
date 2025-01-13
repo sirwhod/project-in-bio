dbb5d445493f29dfe5d23be6901fd51b71aa00c8
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-01-12 22:22:45 -0300

    fix: corrige tipagem do ref

--- app/hooks/useOnClickOutside.ts
+++ app/hooks/useOnClickOutside.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 
 export function useOnClickOutside(
-  ref: React.RefObject<HTMLDivElement>,
+  ref: React.RefObject<HTMLDivElement | null>,
   handler?: (event: MouseEvent | TouchEvent) => void
 ) {
   useEffect(() => {
