efe2ac5adfb78141a934ae411ecebb8e5b0c6b39
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-01-20 20:34:02 -0300

    class: Botões Redes Sociais

--- app/components/commons/user-card/edit-social-links.tsx
+++ app/components/commons/user-card/edit-social-links.tsx
@@ -7,16 +7,23 @@ import { useParams, useRouter } from "next/navigation"
 import createSocialLinks from "@/app/actions/create-social-links"
 import {TextInput} from "../../ui/text-input"
 
-export function EditSocialLinks() {
+export function EditSocialLinks({ socialMedias }:{
+  socialMedias?: {
+    github: string, 
+    instagram: string, 
+    linkedin: string, 
+    twitter: string
+  }
+}) {
   const router = useRouter()
 
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [isSavingSocialLinks, setIsSavingSocialLinks] = useState(false)
 
-  const [github, setGithub] = useState("")
-  const [instagram, setInstagram] = useState("")
-  const [linkedin, setLinkedin] = useState("")
-  const [twitter, setTwitter] = useState("")
+  const [github, setGithub] = useState(socialMedias?.github ?? "")
+  const [instagram, setInstagram] = useState(socialMedias?.instagram ?? "")
+  const [linkedin, setLinkedin] = useState(socialMedias?.linkedin ?? "")
+  const [twitter, setTwitter] = useState(socialMedias?.twitter ?? "")
 
   const { profileId } = useParams()
 
