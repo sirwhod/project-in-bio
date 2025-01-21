76b7b748ea4dd5f3c882b7d69b6afdbd2f3e1338
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2025-01-20 21:29:24 -0300

    class: Editando os Dados de Perfil

--- app/components/commons/user-card/edit-user-card.tsx
+++ app/components/commons/user-card/edit-user-card.tsx
@@ -131,7 +131,7 @@ export function EditUserCard({
             </div>
           </div>
           <div className="flex gap-4 justify-end">
-            <button className="font-bold text-white">Voltar</button>
+            <button onClick={() => setIsModalOpen(false)} className="font-bold text-white">Voltar</button>
             <Button onClick={handleSaveProfile} disabled={isSavingProfile}>
               Salvar
             </Button>
